cd4f71b6ed7c9f3bfa927dd8e195716baef64e73
Tutku Ince <tutku.ince@outlook.com>
2023-09-05 18:44:54 +0300

    create class

---
+++ 04-classes/01-class-definition/Customer.js
@@ -0,0 +1,10 @@
+var Customer = /** @class */ (function () {
+    function Customer() {
+    }
+    return Customer;
+}());
+// let's create an instance 
+var customer = new Customer();
+customer.firstName = 'Tutku';
+customer.lastName = 'Ince';
+console.log(customer);
---
+++ 04-classes/01-class-definition/Customer.ts
@@ -0,0 +1,11 @@
+class Customer {
+    firstName: string;
+    lastName: string;
+}
+
+// let's create an instance 
+let customer = new Customer();
+customer.firstName = 'Tutku';
+customer.lastName = 'Ince';
+
+console.log(customer);
