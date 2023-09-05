f23cb48acd5b9b66d7ff58b0d143a5cf49b43be1
Tutku Ince <tutku.ince@outlook.com>
2023-09-05 16:22:31 +0300

    defining variables

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.9;
+var firstName = 'Tutku';
+var lastName = 'Ince';
+console.log(found);
+console.log("The grade is ".concat(grade));
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 88.9;
+let firstName: string = 'Tutku';
+let lastName: string = 'Ince';
+
+console.log(found);
+console.log(`The grade is ${grade}`);
+console.log(`Hi ${firstName} ${lastName}`);
