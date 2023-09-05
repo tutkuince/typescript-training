5698be6ac3a8ec2b406a3fa3e8de507453954695
Tutku Ince <tutku.ince@outlook.com>
2023-09-05 16:54:32 +0300

    using foreach loop

---
+++ 03-loops-and-arrays/sports.js
@@ -0,0 +1,10 @@
+var sports = ["Golf", "Cricket", "Tennis", "Swimming"];
+for (var _i = 0, sports_1 = sports; _i < sports_1.length; _i++) {
+    var sport = sports_1[_i];
+    if (sport === "Cricket") {
+        console.log("".concat(sport, " is my favorite sport"));
+    }
+    else {
+        console.log(sport);
+    }
+}
---
+++ 03-loops-and-arrays/sports.ts
@@ -0,0 +1,9 @@
+let sports: string[] = ["Golf", "Cricket", "Tennis", "Swimming"];
+
+for(let sport of sports) {
+    if(sport === "Cricket") {
+        console.log(`${sport} is my favorite sport`);
+    } else {
+        console.log(sport);
+    }
+}
