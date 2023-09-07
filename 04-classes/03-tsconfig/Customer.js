be22b9c6ede3bd206c37565c54dfb51b60e8640f
Tutku Ince <tutku.ince@outlook.com>
2023-09-07 16:24:21 +0300

    compiler options with tsconfig.json

---
+++ 04-classes/03-tsconfig/Customer.js
@@ -0,0 +1,34 @@
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    Object.defineProperty(Customer.prototype, "firstName", {
+        // :string => return type
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Customer;
+}());
+// let's create an instance 
+var customer = new Customer('Tutku', 'Ince');
+// customer.firstName = 'Tutku';
+// customer.lastName = 'Ince';
+console.log(customer.firstName);
+console.log(customer.lastName);
